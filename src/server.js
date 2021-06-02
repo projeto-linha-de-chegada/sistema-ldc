ca5f8a566862311f81a0c427b1fa2ab130d4fb0a
felipesantos <felipesantosswengineer@gmail.com>
2021-06-02 14:12:11 -0300

    async test

--- src/server.js
+++ src/server.js
@@ -1,15 +1,29 @@
-const {Pool, Client} = require('pg')
-const connectionString = 'postgressql://postgres:lollol@localhost:5432/postgres'
+const {Pool, Client} = require('pg');
+const connectionString = 'postgressql://postgres:lollol@localhost:5432/postgres';
 
 const client = new Client({
     connectionString:connectionString
-})
+});
 
-client.connect()
+client.connect();
+
+var dados;
 
 client.query('SELECT * from usuarios',(err,res)=>{
-    //console.log(err,res)
-    console.log(res.rows)
-    client.end()
-})
+    
+    if(err){
+        console.log(err.stack);
+    }
+    else{ 
+        console.log(res.rows);
+        dados = res.rows[0].nome;
+    }
+    client.end();
+});
 
+console.log(dados);
+
+function verificaConteudoDosDados(){
+    console.log(dados);
+}
+setInterval(verificaConteudoDosDados,10000);
