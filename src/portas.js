6cd76e3120cdfa69b6d204af0cf2e05ff3ae56a5
Felipe Santos <felipesantosswengineer@gmail.com>
2021-09-09 13:32:00 -0300

    updat

--- src/portas.js
+++ src/portas.js
@@ -1,8 +1,8 @@
 export default function portas(){
-    //var serverHost = "http://localhost:5000";
-    //var clientHost = "http://localhost:3000";
-    var serverHost = "http://10.0.0.106:5000";
-    var clientHost = "http://10.0.0.106:3000";
+    var serverHost = "http://localhost:5000";
+    var clientHost = "http://localhost:3000";
+    //var serverHost = "http://10.0.0.106:5000";
+    //var clientHost = "http://10.0.0.106:3000";
 
     return {serverHost, clientHost};
 }
